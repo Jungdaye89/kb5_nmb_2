3b07960f384ea3eb194701eeac50fdfb7d06274f
Jungdaye89 <jungyj0809@gamil.com>
2024-06-13 21:34:19 +0900

    가계부 크기 수정

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,19 +1,12 @@
 <script setup>
-import CashDetail from '@/pages/Cash/CashDetail.vue';
-import Cash from '@/pages/Cash/Cash.vue';
-import { useDataStore } from '@/stores/db.js';
-import {
-  computed,
-  onMounted,
-  onUpdated,
-} from 'vue';
-import {
-  RouterLink,
-  RouterView,
-} from 'vue-router';
-import Home from '@/pages/Home.vue';
-import Header from '@/components/Header.vue';
-import MonthlyReport from '@/pages/MonthlyReport/MonthlyReport.vue';
+import CashDetail from "@/pages/Cash/CashDetail.vue";
+import Cash from "@/pages/Cash/Cash.vue";
+import { useDataStore } from "@/stores/db.js";
+import { computed, onMounted, onUpdated } from "vue";
+import { RouterLink, RouterView } from "vue-router";
+import Home from "@/pages/Home.vue";
+import Header from "@/components/Header.vue";
+import MonthlyReport from "@/pages/MonthlyReport/MonthlyReport.vue";
 
 // 데이터 불러오기
 const dataStore = useDataStore();
@@ -22,20 +15,20 @@ const requestAPI = dataStore.requestAPI;
 requestAPI();
 
 // 추가 버튼
-import { useRouter } from 'vue-router';
+import { useRouter } from "vue-router";
 const router = useRouter();
 const addHandler = () => {
-  router.push('/Cash');
+  router.push("/Cash");
 };
 
 // Button css
-const $bg = '#fff';
-const $text = '#382b22';
-const $lightPink = '#fff0f0';
-const $pink = '#ffe9e9';
-const $darkPink = '#f9c4d2';
-const $pinkBorder = '#b18597';
-const $pinkShadow = '#ffe3e2';
+const $bg = "#fff";
+const $text = "#382b22";
+const $lightPink = "#fff0f0";
+const $pink = "#ffe9e9";
+const $darkPink = "#f9c4d2";
+const $pinkBorder = "#b18597";
+const $pinkShadow = "#ffe3e2";
 </script>
 
 <template>
@@ -43,16 +36,14 @@ const $pinkShadow = '#ffe3e2';
     <router-view></router-view>
   </div>
 
-  <button class="fastbutton" @click="addHandler">
-    가계부
-  </button>
+  <button class="fastbutton" @click="addHandler">가계부</button>
 </template>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css?family=Rubik:700&display=swap');
+@import url("https://fonts.googleapis.com/css?family=Rubik:700&display=swap");
 
 .page-container {
-  font-family: 'Rubik', sans-serif;
+  font-family: "Rubik", sans-serif;
   font-size: 1rem;
   line-height: 1.5;
   display: flex;
@@ -78,19 +69,18 @@ button.fastbutton {
   font-weight: 600;
   color: #382b22;
   text-transform: uppercase;
-  padding: 1.25em 2em;
+  padding: 0.3em;
   background: #fbda6b; /* Top color */
   border: 2px solid #776c60; /* Border color */
   border-radius: 0.75em;
   transform-style: preserve-3d;
-  transition: transform 150ms
-      cubic-bezier(0, 0, 0.58, 1),
+  transition: transform 150ms cubic-bezier(0, 0, 0.58, 1),
     background 150ms cubic-bezier(0, 0, 0.58, 1);
 }
 
 button.fastbutton::before {
   position: absolute;
-  content: '';
+  content: "";
   width: 100%;
   height: 100%;
   top: 0;
@@ -99,11 +89,9 @@ button.fastbutton::before {
   bottom: 0;
   background: #ffc000; /* Bottom color */
   border-radius: inherit;
-  box-shadow: 0 0 0 2px #776c60,
-    0 0.625em 0 0 #ffe8a2; /* Shadow color */
+  box-shadow: 0 0 0 2px #776c60, 0 0.625em 0 0 #ffe8a2; /* Shadow color */
   transform: translate3d(0, 0.75em, -1em);
-  transition: transform 150ms
-      cubic-bezier(0, 0, 0.58, 1),
+  transition: transform 150ms cubic-bezier(0, 0, 0.58, 1),
     box-shadow 150ms cubic-bezier(0, 0, 0.58, 1);
 }
 
@@ -113,8 +101,7 @@ button.fastbutton:hover {
 }
 
 button.fastbutton:hover::before {
-  box-shadow: 0 0 0 2px #776c60,
-    0 0.5em 0 0 #ffc000;
+  box-shadow: 0 0 0 2px #776c60, 0 0.5em 0 0 #ffc000;
   transform: translate3d(0, 0.5em, -1em);
 }
 
