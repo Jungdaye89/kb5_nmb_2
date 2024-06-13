853823e1d4b8cc015de987007e73ac4ce75ce443
창길이 <sbh9834@gmail.com>
2024-06-13 09:33:40 +0900

    수정 삭제 오류 해결

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,20 @@ import CashDetail from "@/pages/Cash/CashDetail.vue";
 import Cash from "@/pages/Cash/Cash.vue";
 import { useDataStore } from "@/stores/db.js";
 import { computed, onMounted, onUpdated } from "vue";
-import { RouterView } from "vue-router";
+import { RouterView, useRouter } from "vue-router";
 
 // 데이터 불러오기
 const dataStore = useDataStore();
 const requestAPI = dataStore.requestAPI;
 
 requestAPI();
+const router = useRouter();
+function goToCash() {
+  return router.push(`/Cash`);
+}
 </script>
 
-<template><RouterView></RouterView></template>
+<template>
+  <button @click="goToCash()">캐시</button>
+  <RouterView></RouterView>
+</template>
